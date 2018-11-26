a0ebd0160af981743c1d34fe286566c18e0f2fa6
kunleplus <45173093+kunleplus@users.noreply.github.com>
2018-11-26 12:57:07 +0100

    Add files via upload

---
+++ Quadratic.py
@@ -0,0 +1,30 @@
+import math
+
+
+print(2+2)
+print (2*2)
+print(2**4)
+pow(2,4)
+print(14/2)
+math.sqrt(25)
+
+x=int(input("give me a number"))
+y=int(input("give me a number"))
+print(x**y)
+
+a=input("Enter the coefficeients of a")
+b=input("Enter the coefficeients of b")
+c=input("Enter the coefficeients of c")
+d=b**2-4*a*c 
+d<0
+print ("this equation has no real solution")
+d==0
+x=(-b+math.sqrt(b**2-4*a*c))/2*a
+print ("This equationhas no real solutions:")
+x1=(-b+math.sqrt(b**2-4*a*c))/2*a
+x2=(-b-math.sqrt(b**2-4*a*c))/2*a
+print ("This equation has two solitions:", x1,"and", x2)
+        
+
+
+
